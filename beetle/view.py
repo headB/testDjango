186a9c9fb79859353b5dfcf6ad53bd131246b92f
kuman-lenovo <775121173@qq.com>
2017-09-28 21:29:08 +0800

    使用了MVC结构。

--- beetle/view.py
+++ beetle/view.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello world !I am kumanxuan!! ")
+    #return HttpResponse("Hello world !I am kumanxuan!! ")
+    return render(request,'test.html')
