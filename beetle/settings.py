64eadf2d87315fd98ba70227e5607c4cc25e4062
kumanxuan <775121173@qq.com>
2017-09-29 20:17:13 +0800

    modified the setting that add mysql connect

--- beetle/settings.py
+++ beetle/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'beetle.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': 'kumanxuan@gzitcast',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
