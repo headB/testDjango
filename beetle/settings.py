6011458db7ac2f5f82227e5358202ba227a0f602
kuman-lenovo <775121173@qq.com>
2017-09-30 11:35:27 +0800

    可以连接数据库了，方法就是在项目下面，使用mange.py来设置的。django的结构应该是这样。

--- beetle/settings.py
+++ beetle/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'testModel',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'test',
+        'NAME': 'beetle',
         'USER': 'root',
         'PASSWORD': 'kumanxuan@gzitcast',
         'HOST': 'localhost',
--- beetle/urls.py
+++ beetle/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.contrib import admin
 
 from . import view
 
 urlpatterns = [
     url(r'^beetle$', view.hello),
+    url(r'^admin/', admin.site.urls),
 ]
