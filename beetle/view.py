2d039011878492b956ddaf2352ec45ba1bd9f1a1
kuman-lenovo <775121173@qq.com>
2017-10-02 22:54:08 +0800

    测试MTV结构，然后简单通过V（控制器）赋值，然后在T（模板）输出变量。

--- beetle/view.py
+++ beetle/view.py
@@ -2,5 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
+
     #return HttpResponse("Hello world !I am kumanxuan!! ")
-    return render(request,'test.html')
+
+    #return render(request,'test.html')
+    methodC = {}
+    if (request.method=='GET'):
+        methodC['method']='这个是一一个Get处理方法'
+    else:
+        methodC['method']='这个是一一个Post处理方法'
+
+    return render(request,'test.html',methodC)
+
+##哦哦，原来这个地方的VIEW是相当于php里面的MVC结构里面的Controller啦。
+def TestModelSql(request):
+    pass
